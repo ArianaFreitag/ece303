a077a00df451b667412d9a6cc3459411ab554f5c
hyungseokyoon <47166576+hyungseokyoon@users.noreply.github.com>
2020-04-27 14:33:50 -0400

    Add files via upload

---
+++ 2020/yoon_util_sender.py
@@ -0,0 +1,14 @@
+import logging
+import socket
+import channelsimulator
+import utils
+import sys
+import math
+
+partitionnum = int(math.ceil(len(self.data)/float(packetsize))
+partitioncount = 0
+
+def makePKT(self, data, packetnum, packetsize):
+	for i in range(self.partitionnum):
+		partitioncount = partitioncount + 1
+		
